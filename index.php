b12c107f1c2d65bc2b1dae68746e6013f102a9b7
Sanghyun Lee <doltingi@gmail.com>
2013-02-06 01:33:53 -0500

    FFFFFFFFFFFFFFFFFFFFFFFFFFFFF

---
+++ index.php
@@ -0,0 +1,15 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+	<head>
+		<title>Simon and Jenny's Wedding</title>
+		<style type="text/css">
+			.container {
+				width: 800px;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			blah blah blah blah
+		</div>
+	</body>
+</html>
