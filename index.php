d58db1848a8e520762e773d6477a83b23f0455f6
Sanghyun Lee <doltingi@gmail.com>
2013-02-06 15:57:44 -0500

    changed index.php to handle Validator 4.1 Strict
    and also style.css added

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
-<!DOCTYPE html>
-<html>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>' ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+>>>>>>> Very simple update
 	<head>
 		<title>Simon and Jenny's Wedding</title>
-		<style type="text/css">
-			.container {
-				width: 800px;
-			}
-		</style>
+		<link rel="stylesheet" type="text/css" href="style.css" />
 	</head>
 	<body>
 		<div class="container">
 			blah blah blah blah
+		</div>
+		<div class="login_form">
+			<div class="title">Guestbook</div><br />
+			<form method="get" action="">
+				<div class="login_row">
+					<div class="login_left">Username</div>
+					<div class="login_right"><input class="login_textfield" type="text" /></div>
+				</div>
+				<div class="login_row">
+					<div class="login_left">Password</div>
+					<div class="login_right"><input class="login_textfield" type="password" /></div>
+				</div>
+				<div class="login_row">
+					<input type="submit" value="Log In" />&nbsp;<input type="button" value="reset" />
+				</div>
+			</form>
+
 		</div>
 	</body>
 </html>
