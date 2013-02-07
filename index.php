6a18cd9bbcc3e216da795c2751b6878cc24dd9c8
Sanghyun Lee <doltingi@gmail.com>
2013-02-06 21:57:17 -0500

    bootstrap installed
    index meeting HTML5

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>' ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
->>>>>>> Very simple update
+<!DOCTYPE html>
+<html>
 	<head>
+		<meta charset="UTF-8">
 		<title>Simon and Jenny's Wedding</title>
 		<link rel="stylesheet" type="text/css" href="style.css" />
 	</head>
