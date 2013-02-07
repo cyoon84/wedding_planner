c288b7ac8a983fb3c9a7945e1a5f75a161fe883e
Sanghyun Lee <doltingi@gmail.com>
2013-02-06 23:20:52 -0500

    library added, indexing passing authentication
    GOOD!

--- index.php
+++ index.php
@@ -1,30 +1,96 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Simon and Jenny's Wedding</title>
-		<link rel="stylesheet" type="text/css" href="style.css" />
-	</head>
-	<body>
-		<div class="container">
-			blah blah blah blah
-		</div>
-		<div class="login_form">
-			<div class="title">Guestbook</div><br />
-			<form method="get" action="">
-				<div class="login_row">
-					<div class="login_left">Username</div>
-					<div class="login_right"><input class="login_textfield" type="text" /></div>
-				</div>
-				<div class="login_row">
-					<div class="login_left">Password</div>
-					<div class="login_right"><input class="login_textfield" type="password" /></div>
-				</div>
-				<div class="login_row">
-					<input type="submit" value="Log In" />&nbsp;<input type="button" value="reset" />
-				</div>
-			</form>
+<?php include('library.php');
+$login_result = false;
 
-		</div>
-	</body>
+if ($_POST) {
+	if($login_result = login_to_db($_POST["uname"], $_POST["pw"])) {
+		mysql_close($db_con);
+	}
+}
+if ($login_result && !$_SESSION["role"]) {
+	header("Location: admin.php");
+} else {
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo(TITLE . " :: " . LOGIN) ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Sanghyun Lee">
+    <meta name="author" content="Chulhee Yoon">
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+    <script src="http://code.jquery.com/jquery-1.8.0.js"></script>
+    <link href="style.css" rel="stylesheet">
+	<link href="css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+      }
+
+      .form-signin {
+        max-width: 300px;
+        padding: 19px 29px 29px;
+        margin: 0 auto 20px;
+        background-color: #fff;
+        border: 1px solid #e5e5e5;
+        -webkit-border-radius: 5px;
+           -moz-border-radius: 5px;
+                border-radius: 5px;
+        -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+           -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                box-shadow: 0 1px 2px rgba(0,0,0,.05);
+      }
+      .form-signin .form-signin-heading,
+      .form-signin .checkbox {
+        margin-bottom: 10px;
+      }
+      .form-signin input[type="text"],
+      .form-signin input[type="password"] {
+        font-size: 16px;
+        height: auto;
+        margin-bottom: 15px;
+        padding: 7px 9px;
+      }
+
+    </style>
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="ico/favicon.png">
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <input type="text" class="input-block-level" placeholder="Email address" name="uname" />
+        <input type="password" class="input-block-level" placeholder="Password" name="pw" />
+        <label class="checkbox">
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+        <button class="btn btn-large btn-primary" type="submit">Sign in</button>&nbsp;&nbsp;
+        <button class="btn btn-large btn-primary" type="submit">Create New</button>
+      </form>
+    </div>
+    <script src="js/jquery.js"></script>
+  </body>
 </html>
+<?php
+}
+?>
---
+++ library.php
@@ -0,0 +1,58 @@
+<?php
+
+	// connects to the database
+	$db_name = "fssto651_weddingPlanner";
+	$db_username = "fssto651";
+	$db_pwd = "Fsstoronto123";
+	$db_con = mysql_connect("localhost", $db_username, $db_pwd) or die("There was an error connecting");
+	mysql_select_db($db_name) or die("There was an error in selecting the database.");
+	session_start();
+
+	define('TITLE', 'Simon and Jenny\'s Wedding');
+	define('LOGIN', 'Log In');
+	define('RSVP', 'RSVP');
+	define('GUESTMGR', 'Guest Manager');
+	define('TY', 'Thank You');
+	define('VIEW', 'View Guests');
+	define('GR', 'Groom');
+	define('BR', 'Bride');
+	define('NOTATND', 'Not Attending');
+
+	/* Functions for Log In Start */
+
+	// checks if there exists a session id uname and returns result
+	function check_login() {
+		return isset($_SESSION["uname"]);
+	}
+	
+	// log in procedure
+	function login_to_db($uname, $pword) {
+		$success = false;
+		$role = 0;
+
+		// extracts a row from users table that matches user name from the input.
+		// the input has been securely enhanced by using mysql_real_escape_string()
+		$qry = sprintf("SELECT password, role FROM weddingPlanner WHERE email='%s'", mysql_real_escape_string($uname));
+		$qry_res = mysql_query($qry) or die ("There was an error selecting a row from user table.<br />".mysql_error());
+		
+		// if there is a row extracted, there is a user id that matches the input by the user
+		if (mysql_num_rows($qry_res) == 1) {
+			$row = mysql_fetch_array($qry_res);
+			// holds values from the extracted row
+			$encrypted_pw = $row[0];
+			$role = $row[1];
+			// compare the encrypted password
+			if ($encrypted_pw == $pword) {
+				$success = true;
+				// sets the attempt to true, and stores session values accordingly
+				setcookie ("PHPSESSID", session_id(), time() + 600);
+				$_SESSION["uname"] = $uname;
+				$_SESSION["role"] = $role;
+			}
+		}
+
+		// otherwise, login attempt failed
+		return $success;
+	}
+	/* Functions for Log In End */
+?>
