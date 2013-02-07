624ac4a99a8a81882a5b4dce43d680b80e6a9d5f
Chulhee Yoon <chulhee.y@gmail.com>
2013-02-06 22:19:23 -0500

    bootstrap examples

---
+++ admin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+<link href="css/bootstrap-responsive.css" rel="stylesheet">
+<script src="http://code.jquery.com/jquery-1.8.0.js"></script>
+<script src="js/bootstrap.js"></script>
+</head>
+<body>
+<div class="span10" id="adminOuter">
+	<h1> View Guests List </h1>
+	<div class="row">
+		<div class="span4">
+			<h3>Simon's guests</h3>
+		</div>
+
+		<div class="span4">
+			<h3>Jenny's guests</h3>
+		</div>
+	</div>
+
+	<div id="noShow" class="span8">
+		<h3>Not attending</h3>
+	</div>
+</div>
+</body>
+</html>
